0bc2117135b57e5c652bd1f5dcd0648326519c03
Cleverson Sucalit <cleversonsucalit@gmail.com>
2023-10-11 00:43:13 +0800

    Updated top buttons

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,45 +8,49 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        toolbarHeight: 100,
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        leading: GestureDetector(
-          onTap: (){},
-          child: Container(
-            margin: EdgeInsets.fromLTRB(25, 10, 10, 10),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(5)
-            ),
-            child: Image.asset(
-              'assets/icons/menu_icon.png',
-              height: 60,
-              width: 60,
-            ),
+      appBar: topBarButtons()
+    );
+  }
+
+  AppBar topBarButtons() {
+    return AppBar(
+      toolbarHeight: 100,
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      leading: GestureDetector(
+        onTap: (){},
+        child: Container(
+          margin: EdgeInsets.fromLTRB(25, 10, 10, 10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5)
           ),
-        ),
-        actions: [
-          GestureDetector(
-          onTap: (){},
-          child: Container(
-            margin: EdgeInsets.fromLTRB(10, 10, 25, 10),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(5)
-            ),
-            child: Image.asset(
-              'assets/icons/top_icon.png',
-              height: 35,
-              width: 35,
-            ),
+          child: Image.asset(
+            'assets/icons/menu_icon.png',
+            height: 60,
+            width: 60,
+          )
+        )
+      ),
+      actions: [
+        GestureDetector(
+        onTap: (){},
+        child: Container(
+          margin: EdgeInsets.fromLTRB(10, 10, 25, 10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5)
           ),
-        ),
-          ],
-      )
+          child: Image.asset(
+            'assets/icons/top_icon.png',
+            height: 35,
+            width: 35,
+          ),
+        )
+        )
+      ]
     );
   }
 }
