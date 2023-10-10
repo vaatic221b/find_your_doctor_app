70771c346630c88f666946750555588fe84aabbc
Cleverson Sucalit <cleversonsucalit@gmail.com>
2023-10-11 01:58:42 +0800

    searchBar done

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Find Your Doctor App, Sucalit - Parales',
+      title: 'Find Your Doctor App',
       debugShowCheckedModeBanner: false,
       home: HomePage()
     );
