281d862edd986e989bc2972d07c927db5d7b57b3
Cleverson Sucalit <cleversonsucalit@gmail.com>
2023-10-11 22:36:46 +0800

    Please check line 202

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -49,58 +49,6 @@ class _HomePageState extends State<HomePage> {
 
 // METHODS 
 
-  TextField searchBar() //search bar
-  {
-    return TextField(
-      decoration: InputDecoration(
-        filled: true,
-        fillColor: const Color(0xFFF6F6F6),
-        contentPadding: const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
-        hintText: 'Search doctor, medicines, etc',
-        hintStyle: TextStyle(
-          fontFamily: GoogleFonts.lato().fontFamily,
-          color: const Color(0xFFCACCCF),
-          fontSize: 13
-        ),
-        suffixIcon: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 0, 10.0, 0), 
-          child: Image.asset('assets/icons/search_icon.png'),
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide.none
-        )
-      )
-    );
-  }
-
-  RichText topText() //find your doctor text
-  {
-    return RichText(
-            text: TextSpan(
-              children: [             
-                TextSpan(
-                  text: 'Find ',
-                  style: TextStyle(
-                    color: const Color(0xFF25282B), 
-                    fontSize: 35,
-                    fontFamily: GoogleFonts.lato().fontFamily,
-                  )
-                ),
-
-                TextSpan(
-                  text: 'your doctor',
-                  style: TextStyle(
-                    color: const Color(0xFFA0A4A8), 
-                    fontSize: 35,
-                    fontFamily: GoogleFonts.lato().fontFamily,
-                  )
-                )
-              ]
-            )
-          );
-  }
-
   AppBar topBarButtons() //top left menu and top right icon
   {
     return AppBar(
@@ -142,6 +90,58 @@ class _HomePageState extends State<HomePage> {
         )
         )
       ]
+    );
+  }
+
+  RichText topText() //find your doctor text
+  {
+    return RichText(
+            text: TextSpan(
+              children: [             
+                TextSpan(
+                  text: 'Find ',
+                  style: TextStyle(
+                    color: const Color(0xFF25282B), 
+                    fontSize: 35,
+                    fontFamily: GoogleFonts.lato().fontFamily,
+                  )
+                ),
+
+                TextSpan(
+                  text: 'your doctor',
+                  style: TextStyle(
+                    color: const Color(0xFFA0A4A8), 
+                    fontSize: 35,
+                    fontFamily: GoogleFonts.lato().fontFamily,
+                  )
+                )
+              ]
+            )
+          );
+  }
+
+  TextField searchBar() //search bar
+  {
+    return TextField(
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: const Color(0xFFF6F6F6),
+        contentPadding: const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+        hintText: 'Search doctor, medicines, etc',
+        hintStyle: TextStyle(
+          fontFamily: GoogleFonts.lato().fontFamily,
+          color: const Color(0xFFCACCCF),
+          fontSize: 13
+        ),
+        suffixIcon: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 10.0, 0), 
+          child: Image.asset('assets/icons/search_icon.png'),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none
+        )
+      )
     );
   }
 
@@ -195,14 +195,14 @@ class _HomePageState extends State<HomePage> {
         {
           return GestureDetector(
             onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return DoctorDetails(doctorName: doctors[index].name);    //name the class DoctorDetails extends stateless widget etc etc so this works. Should open screen 2
-                  },
-                ),
-              );
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(
+              //     builder: (context) {
+              //       return DoctorDetails(doctorName: doctors[index].name);    //name the class DoctorDetails extends stateless widget etc etc so this works. Should open screen 2
+              //     },
+              //   ),
+              // );
             },
             child: Container(
               height: 100,
